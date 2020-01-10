cfdd5cee887549feeff217c8a2c9736f2c5827ae
travis skyles <travis.skyles@gmail.com>
2020-01-09 21:43:19 -0800

    fixed Data model require on products.js and created new product

--- models/categories/products.js
+++ models/categories/products.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const DataModel = require('../memory-data-model.js');
+const DataModel = require('../file-data-model.js');
 
 class Products extends DataModel {
   constructor() {
@@ -8,7 +8,7 @@ class Products extends DataModel {
     this.schema = {
       fields: {
         category_id: { type: 'string', required: true },
-        price: { type: 'string', required: true },
+        price: { type: 'number', required: true },
         weight: { type: 'number' },
         quantity_in_stock: { type: 'number', required: true },
       },
@@ -16,5 +16,14 @@ class Products extends DataModel {
   }
 
 }
+
+const laptops = new Products();
+
+laptops.create({
+  category_id: 'laptops',
+  price: 200,
+  weight: 4,
+  quantity_in_stock: 50,
+});
 
 module.exports = Products;
