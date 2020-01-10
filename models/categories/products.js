d6a58c427b85111c5e2b8a58533076e78f062da4
travis skyles <travis.skyles@gmail.com>
2020-01-09 16:55:57 -0800

    created Products

---
+++ models/categories/products.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const DataModel = require('../memory-data-model.js');
+
+class Products extends DataModel {
+  constructor() {
+    super();
+    this.schema = {
+      fields: {
+        category_id: { type: 'string', required: true },
+        price: { type: 'string', required: true },
+        weight: { type: 'number' },
+        quantity_in_stock: { type: 'number', required: true },
+      },
+    };
+  }
+}
+
+module.exports = Products;
